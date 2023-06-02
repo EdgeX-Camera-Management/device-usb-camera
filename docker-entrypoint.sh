b75ca26bd7a439984776c173c363dc251f9096da
Anthony Casagrande <anthony.j.casagrande@intel.com>
2023-06-02 14:30:45 -0700

    feat: allow user to disable rtsp server on startup via env var
    Signed-off-by: Anthony Casagrande <anthony.j.casagrande@intel.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/dumb-init /bin/sh
+# shellcheck shell=sh
 
-echo "Run rtsp-simple-server..."
-/rtsp-simple-server &
+# ENABLE_RTSP_SERVER is set to 'true' by default. If it has
+# been set to anything else, the rtsp server will not be started
+ENABLE_RTSP_SERVER="${ENABLE_RTSP_SERVER:-true}"
+
+if [ "$ENABLE_RTSP_SERVER" = "true" ]; then
+  echo "Run rtsp-simple-server..."
+  /rtsp-simple-server &
+else
+  echo "RTSP server is disabled."
+fi
 
 echo "Run device-usb-camera..."
-/device-usb-camera $@
+/device-usb-camera "$@"
